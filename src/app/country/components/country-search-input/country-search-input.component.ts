ab45136f53afc748bec810a6c3e15ce45c8f08fe
IrvingJT1 <irvinjt@gmail.com>
2025-07-05 21:27:02 -0600

    update

--- src/app/country/components/country-search-input/country-search-input.component.ts
+++ src/app/country/components/country-search-input/country-search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -9,6 +9,26 @@ export class CountrySearchInputComponent {
 
   nameChange = output<string>();
   placeholder = input('Buscar');
+  inputValue = signal<string>('');
+
+  //Se establece un effect, similar al useEffect de React
+  //al usar inputValue() y ver que es un signal, el effect ya sabe que debe revisar si hay algun cambio en esa variable
+  //se establece un setTimeout para asegurar un emit al componente padre cada cierto tiempo, en este caso 500ms
+  //se usa la función asociada a effect que se llama onCleanup, y esta se usa para hacer "limpieza" cada vez que se requiera
+  //las únicas formas en que se ejecuta es cuando se destruye el componente o cada vez que el signal inputValue() cambie de valor
+  //se define el método dentro del effect y en él solo se usa la función clearTimeout para cancelar el objeto timeout que se ejecuta cada 500ms
+  //de esta forma se asegura que hasta que el usuario deje de escribir, la llamada a servicio se llevará a cabo 
+  debounceEffect = effect((onCleanup)=>{
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.nameChange.emit(value);
+    }, 500);
+
+    onCleanup(()=>{
+      clearTimeout(timeout);
+    });
+  });
 
   onSearch = (value:string) => {
     this.nameChange.emit(value);
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable, catchError, throwError, delay } from 'rxjs';
+import { map, Observable, catchError, throwError, delay, of, tap } from 'rxjs';
 
 import { CountryMapper } from '../mappers/country.mapper';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
 import { Country } from '../interfaces/country.interface';
+import { RouteConfigLoadEnd } from '@angular/router';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -15,18 +16,45 @@ export class CountryService {
 
   private http = inject(HttpClient);
 
+  private queryCacheCapital = new Map<string, Country[]>();
+
+  private queryCacheCountry = new Map<string, Country[]>();
+
   searchByCapital = ( query : string ):Observable<Country[]>=>{
     query = query.toLowerCase();
+    
+    //Se verifica si el objeto de tipo Map (que es como un Set, pero guarda pares 'key':value)
+    //contiene un query como el que se solicita, o sea si el query de la consulta al servicio está incluido
+    //en alguno de los pares 'key':value del objeto, si es así se regresa lo que ya se tenía en ese objeto Map que se está usando como caché
+    //en caso contrario se usa un null coalescent (??) para indicar que se retornará un arreglo vacío en caso de que la consulta no haya traido nada desde el principio
+    //de esta forma evitamos múltiples llamadas innecesarias al servicio
+    if( this.queryCacheCapital.has(query) )
+    {
+      return of(this.queryCacheCapital.get(query) ?? []);
+    }
+
+    console.log('Llegando al servidor por ' + query)
 
     //En el get se indica que tipo de dato se va a retornar, para evitar el uso de any
     //y el subscribe sepa que tipo de dato es el que va a recibir
 
-    // return this.http.get<RESTCountry[]>(`${API_URL}/capital/${ query }`);
+    //se usa la función tap de rxjs para hacer un efecto secundario sin necesidad de alterar lo ya obtenido por la
+    //llamada a servicio, en este caso se crea el par 'key':value del objeto Map que se llama queryCacheCapital
+    //para tener su registro en memoria en forma de query:Country[], donde el query es el parámetro de búsqueda
+    //y el Country[] un objeto que puede trar un array de Countries con datos o vacío
+
+    //por ejemplo "ardf":[] o "mex":[contenido de países cuya capital incluya mex]
+
+    //lo demás contnúa con su flujo normal para procesar la información obtenida
+
 
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${ query }`)
     .pipe(
       map((item) => {
         return CountryMapper.mapRestCountryItemsToCountryArray(item);
+      }),
+      tap((countries)=>{
+        this.queryCacheCapital.set(query,countries);
       }),
       catchError(error => {
         console.log('Error fetching ', error);
@@ -40,12 +68,29 @@ export class CountryService {
 
     query= query.toLowerCase();
 
+    if( this.queryCacheCountry.has(query))
+    {
+      return of(this.queryCacheCountry.get(query) ?? []);
+    }
+
+    // if( this.queryCacheCapital.has(query) )
+    // {
+    //   return of(this.queryCacheCapital.get(query) ?? []);
+    // }
+    // tap((countries)=>{
+    //     this.queryCacheCapital.set(query,countries);
+    //   }),
+
+    console.log('Llega al servidor con el query de paises')
     return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`)
     .pipe(
       map((item)=>{
         return CountryMapper.mapRestCountryItemsToCountryArray(item);
       }),
       delay(2000),
+      tap((countries)=>{
+        this.queryCacheCountry.set(query, countries);
+      }),
       catchError(error => {
         console.log('Error fetching ', error);
 
